81609f6b3faebcc10b3e6698cd3baa6549c169de
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-12-07 21:33:20 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,7 +20,7 @@ SERVER_CONFIG="server {
 	}
 
 	if (\$request_filename ~ redirect_me) {
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+		rewrite ^ https://abdessamad.com/bluepeno/models permanent;
 	}
 
 	location = /404.html {
